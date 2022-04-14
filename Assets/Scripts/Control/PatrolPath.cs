a22f99d19474e52134c763d5a9bbb1cc98b5e877
prashant-naidu <naidu.s.prashant@gmail.com>
2022-04-14 15:14:33 -0700

    adjusts some boxes in scene and creates patrol path

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        private void OnDrawGizmos()
+        {
+            for (int i=0; i < transform.childCount-1; i++)
+            {
+
+            }
+        }
+    }
+}
