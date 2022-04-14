448b7839eea32f73990a5f905ce948383b20c828
prashant-naidu <naidu.s.prashant@gmail.com>
2022-04-14 15:31:19 -0700

    visualizing patrol path

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -6,14 +6,29 @@ namespace RPG.Control
 {
     public class PatrolPath : MonoBehaviour
     {
-        private static float m_WaypointGizmoRadius = 1f;
+        private static float m_WaypointGizmoRadius = 0.3f;
 
         private void OnDrawGizmos()
         {
-            for (int i=0; i < transform.childCount-1; i++)
+            for (int i=0; i < transform.childCount; i++)
             {
-                Gizmos.DrawSphere(transform.GetChild(i).position, m_WaypointGizmoRadius);
+                Gizmos.DrawSphere(GetWaypoint(i), m_WaypointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(GetNextIndex(i)));
             }
+        }
+
+        private int GetNextIndex(int i)
+        {
+            if (i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
         }
     }
 }
