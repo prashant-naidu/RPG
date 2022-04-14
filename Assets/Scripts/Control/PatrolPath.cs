76ea8b4b6505613cf0da626df7dca735af2c29e7
prashant-naidu <naidu.s.prashant@gmail.com>
2022-04-14 15:18:56 -0700

    patrol path prefab, and remove old target sphere in scene

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -6,11 +6,13 @@ namespace RPG.Control
 {
     public class PatrolPath : MonoBehaviour
     {
+        private static float m_WaypointGizmoRadius = 1f;
+
         private void OnDrawGizmos()
         {
             for (int i=0; i < transform.childCount-1; i++)
             {
-
+                Gizmos.DrawSphere(transform.GetChild(i).position, m_WaypointGizmoRadius);
             }
         }
     }
