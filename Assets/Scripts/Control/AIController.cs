0c674993ba48351a5db65eea804de6b268e4f5f5
prashant-naidu <naidu.s.prashant@gmail.com>
2022-04-14 13:41:35 -0700

    Adds Player tag, AIController that identifies player when at a specified distance

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        public float ChaseDistance = 5f;
+
+        private GameObject m_PlayerGO;
+
+        private void Awake()
+        {
+            m_PlayerGO = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        private void Update()
+        {
+            if (Vector3.Distance(transform.position, m_PlayerGO.transform.position) <= ChaseDistance)
+            {
+                print("Chase");
+            }
+        }
+    }
+}
