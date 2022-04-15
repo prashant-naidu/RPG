c579b32def6c0381c4bc099cb9b9359c7f482370
prashant-naidu <naidu.s.prashant@gmail.com>
2022-04-14 20:58:24 -0700

    trigger the cinematic

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private bool m_HasPlayed = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!m_HasPlayed && other.gameObject.tag == "Player")
+            {
+                m_HasPlayed = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+            
+        }
+    }
+}
