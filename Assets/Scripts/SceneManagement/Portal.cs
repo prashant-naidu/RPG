4c5e714981ab0e84a00ef6681c2314a50d226751
prashant-naidu <naidu.s.prashant@gmail.com>
2022-04-15 00:15:14 -0700

    adds portals

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        public enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
+
+        public int SceneToLoad = -1;
+        public Transform SpawnPoint;
+        public DestinationIdentifier Destination;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            if (SceneToLoad < 0)
+            {
+                Debug.LogError("Scene to load not set!");
+                yield break;
+            }
+
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(SceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            print("scene loaded");
+            Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.Destination != this.Destination) continue;
+
+                return portal;
+            }
+
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
+            playerGO.GetComponent<NavMeshAgent>().Warp(otherPortal.SpawnPoint.position);
+            playerGO.transform.rotation = otherPortal.SpawnPoint.rotation;
+        }
+    }
+}
