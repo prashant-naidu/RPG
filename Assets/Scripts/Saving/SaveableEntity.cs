705f0fb7d70fd9a6fb433b18ffc8683590311736
prashant-naidu <naidu.s.prashant@gmail.com>
2022-04-19 00:34:31 -0700

    adding a saving system

---
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    [ExecuteAlways]
+    class SaveableEntity : MonoBehaviour
+    {
+        [SerializeField] private string m_Id = "";
+
+        //private void Awake()
+        //{
+        //    m_Id = System.Guid.NewGuid().ToString();
+        //}
+
+        private void Update()
+        {
+            if (Application.IsPlaying(gameObject)) return;
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+
+            SerializedObject serializedObject = new SerializedObject(this);
+            SerializedProperty idProperty = serializedObject.FindProperty("m_Id");
+
+            if(string.IsNullOrEmpty(idProperty.stringValue))
+            {
+                idProperty.stringValue = System.Guid.NewGuid().ToString();
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+
+        public string GetUniqueIdentifier()
+        {
+            return m_Id;
+        }
+
+        public object CaptureState()
+        {
+            print("Capturing state for " + GetUniqueIdentifier());
+            return null;
+        }
+
+        public void RestoreState(object state)
+        {
+            print("Restoring state for " + GetUniqueIdentifier());
+        }
+    }
+}
---
+++ Assets/Scripts/Saving/SavingWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        [Header("Dependencies")]
+        [SerializeField] private SavingSystem m_SavingSystem;
+
+        const string defaultSaveFile = "save";
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                m_SavingSystem.Save(defaultSaveFile);
+            }
+
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                m_SavingSystem.Load(defaultSaveFile);
+            }
+        }
+    }
+}
+
+
