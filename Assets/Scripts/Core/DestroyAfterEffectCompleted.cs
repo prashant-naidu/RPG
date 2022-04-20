4b5945131b1e046f004884dff438c786d0409c05
prashant-naidu <naidu.s.prashant@gmail.com>
2022-04-20 15:51:28 -0700

    cleanup impact effect

---
+++ Assets/Scripts/Core/DestroyAfterEffectCompleted.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterEffectCompleted : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem m_ParticleSystem = null;
+
+    private void Update()
+    {
+        if (!m_ParticleSystem.IsAlive())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
