c5445d0fffe487c0805c00ca9db2248a769e3510
prashant-naidu <naidu.s.prashant@gmail.com>
2022-04-24 04:27:49 -0700

    saves experience

--- Assets/Scripts/Attributes/Experience.cs
+++ Assets/Scripts/Attributes/Experience.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RPG.Saving;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,13 +8,23 @@ using UnityEngine;
 
 namespace RPG.Attributes
 {
-    class Experience : MonoBehaviour
+    class Experience : MonoBehaviour, ISaveable
     {
         [SerializeField] private float m_ExperiencePoints = 0;
 
         public void GainExperience(float experience)
         {
             m_ExperiencePoints += experience;
+        }
+
+        public object CaptureState()
+        {
+            return m_ExperiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            m_ExperiencePoints = (float)state;
         }
     }
 }
