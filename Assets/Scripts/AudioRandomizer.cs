9148641809aabdf22c9614fe9908b3592879bbcb
prashant-naidu <naidu.s.prashant@gmail.com>
2022-04-28 19:23:31 -0700

    adds damage audio

---
+++ Assets/Scripts/AudioRandomizer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioRandomizer : MonoBehaviour
+{
+    [SerializeField] private AudioSource m_AudioSource = null;
+    [SerializeField] private AudioClip[] m_AudioClips;
+
+    public void Play()
+    {
+        m_AudioSource.PlayOneShot(m_AudioClips[Random.Range(0, m_AudioClips.Length)]);
+    }
+}
